bd465154267652c2703fa594aacafc67318336e2
mosesochacha <ochachamoses443@gmail.com>
2023-02-06 22:19:11 +0300

    Done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,36 @@
 # Your code here!
+  
+def greet_programmer
+    puts "Hello, programmer!"
+end   
+  
+greet_programmer
+
+
+def greet name = "Naureen"
+    puts "Hello, #{name}!"
+end    
+
+greet
+
+def greet_with_default name = "programmer"
+    puts "Hello, #{name}!"
+end
+
+greet_with_default 
+
+def add num1 , num2
+    num1 + num2
+    end
+
+    add 1 , 2
+
+   def halve (number)
+    if number.class != Integer
+        return nil
+    else
+         number/2
+      end
+     end
+     halve "Hello" 
+    halve 6
